3c96ad38748a73ca638f5685c9e6740c50603de7
Ryebread5555 <Rypetty55@gmail.com>
2023-03-12 11:08:24 -0500

    created helper for dates

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,12 @@
-// export format_date
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+
+    },
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+        return word;
+    }
+}
