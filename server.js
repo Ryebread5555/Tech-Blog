23400448756bcb8a6c1c67604c9dae78a0e8ee95
Ryebread5555 <Rypetty55@gmail.com>
2023-03-12 13:57:50 -0500

    updated server.js

--- server.js
+++ server.js
@@ -5,3 +5,40 @@ const path = require('path');
 
 // helper functions
 const helpers = require('./utils/helpers')
+
+// handlebars
+
+// connect session to sequelize
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// create session
+const sess = {
+    secret: "ThedogIsaGoodboy",
+    cookie: { originalMaxAge: 600000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use (session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// handlbars as default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ fore: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
