89825cb29835ca7317f12d8169acafdd4dd94369
Ryebread5555 <Rypetty55@gmail.com>
2023-03-12 13:58:14 -0500

    created routes for homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,98 @@
+const { Post, User, Comment } = require('../models');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+// render all posts to the homepage
+router.get('/', (req, res) => {
+    console.log(req.sessions);
+
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_text'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts, loggedIn: req.sessions.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// redirect user to homepage after log in
+router.get('/login', (req, res) => {
+    if(req.sessions.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+// render sign up page
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+//will render 1 post to the single-post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_text'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']    
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post was found matching this id' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', { post, loggedIn: req.sessions.loggedIn});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
