760617975809beae95b6bc359bb496b4e594a1bb
Ryebread5555 <Rypetty55@gmail.com>
2023-03-12 20:00:07 -0500

    fixed spelling

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,25 +1,31 @@
 async function newFormHandler(event) {
     event.preventDefault();
-
+  
     const title = document.querySelector('input[name="post-title"]').value;
     const post_text = document.querySelector('textarea[name="post-text"]').value;
-
-    const response = await fetch(`/api/posts`, {
+  
+    try {
+      const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
-            title,
-            post_text
+          title,
+          post_text
         }),
         headers: {
-            'Content-Type': 'applications/json'
+          'Content-Type': 'application/json'
         }
-    });
-
-    if (response.ok) {
+      });
+  
+      if (response.ok) {
         document.location.replace('/dashboard/');
-    } else {
+      } else {
         alert(response.statusText);
+      }
+    } catch (err) {
+      console.error(err);
+      alert('Failed to create post.');
     }
-}
-
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  
