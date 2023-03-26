13ce9a44ce6838f25e8afbd5dd05014d7638c97c
Ryebread5555 <rypetty55@gmail.com>
2023-03-26 13:47:43 -0500

    updated helped

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,10 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
-        res.redirect('./login');
+    console.log(req.session);
+    if (!req.session.user_id) {
+      res.redirect('/login');
     } else {
-        next();
+      next();
     }
-};
+  };  
 
 module.exports = withAuth;
